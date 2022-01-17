5a7c00ecdcda257f6233f1a1948bc73dc1f6f327
goran9999 <gutovicgoran@gmail.com>
2022-01-17 16:23:01 +0100

    kreirana pocetna struktura pametnog ugovora i migracija

---
+++ contracts/TodoList.sol
@@ -0,0 +1,24 @@
+pragma solidity  >=0.4.22 <0.9.0;
+
+
+contract TodoList{
+
+    uint public brojZadataka  = 0;
+
+    // struct Zadatak{
+    //     uint id;
+    //     string tekst;
+    //     bool zavrsen;
+    // }
+
+    // constructor ()  {
+
+    // }
+
+
+
+    // function kreirajZadatak(string memory _tekst) public {
+
+    // }
+
+}
